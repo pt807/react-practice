687c556925668911c28befb8c6972a70bbb097b7
pt807 <pt807@naver.com>
2023-03-08 09:54:33 +0900

    babel #finish

--- 9.babel-practice/ex03/dist/index.js
+++ 9.babel-practice/ex03/dist/index.js
@@ -1,20 +1,19 @@
 // 블록 스코프 변수(es6)
-const users = [{
-  no: 1,
-  name: "마이콜",
-  email: "michol@gmail.com"
-}, {
-  no: 2,
-  name: "둘리",
-  email: "dooly@gmail.com"
-}];
+const users = [
+  {
+    no: 1,
+    name: "마이콜",
+    email: "michol@gmail.com",
+  },
+  {
+    no: 2,
+    name: "둘리",
+    email: "dooly@gmail.com",
+  },
+];
 
 // 객체 분해
-function print({
-  no,
-  name,
-  email
-}) {
+function print({ no, name, email }) {
   console.log(`${no}:${name}:${email}`);
 }
 
