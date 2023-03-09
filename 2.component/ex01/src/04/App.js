7db80c348b28eed439da3868633eedf80e4b4322
pt807 <pt807@naver.com>
2023-03-09 09:42:04 +0900

    component property

--- 2.component/ex01/src/04/App.js
+++ 2.component/ex01/src/04/App.js
@@ -4,7 +4,16 @@ import MyComponent from "./MyComponent";
 function App() {
   return (
     <div id="App">
-      <MyComponent props02={10} />
+      <MyComponent
+        props02={10}
+        props03={true}
+        props04={{ no: 1, name: "둘리" }}
+        props05={["hello", "world", "react.js", "es6"]}
+        props06={() => "함수"}
+        props07={10}
+        props08={[true, false, true, true]}
+        props09={{ no: 1, name: "둘리", email: "dooly@gmail.com" }}
+      />
     </div>
   );
 }
