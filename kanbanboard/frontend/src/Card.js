68832e247c53753ba2f6fad32074a0f1c4ac4434
pt807 <pt807@naver.com>
2023-03-15 13:35:39 +0900

    kanabanboard #finish

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,13 +1,74 @@
 import React, { useState } from "react";
 import styles from "./assets/scss/Card.scss";
 import TaskList from "./TaskList";
+import update from "react-addons-update";
 
 const Card = ({ no, title, description }) => {
   const [showDetail, setShowDetail] = useState(false);
   const [tasks, setTasks] = useState([]);
 
+  const changeTaskDone = async (taskNo, done) => {
+    try {
+      const response = await fetch(`/api/task/${taskNo}`, {
+        method: "put",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `done=${done}`,
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+      if (json.result !== "success") {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      const newTasks = update(tasks, {
+        [tasks.findIndex((task) => task.no === json.data.no)]: {
+          done: {
+            $set: json.data.done,
+          },
+        },
+      });
+      setTasks(newTasks);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   const addTask = async (taskName) => {
-    console.log(taskName);
+    const newTask = {
+      no: null,
+      name: taskName,
+      done: "N",
+      cardNo: no,
+    };
+    try {
+      const response = await fetch("/api/task", {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTask),
+      });
+
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+      if (json.result !== "success") {
+        throw new Error(`${json.result} ${json.message}`);
+      }
+
+      setTasks([json.data, ...tasks]);
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   return (
@@ -51,7 +112,11 @@ const Card = ({ no, title, description }) => {
       {showDetail ? (
         <div className={styles.Card__Details}>
           {description}
-          <TaskList cardNo={no} tasks={tasks} callbackAddTask={addTask} />
+          <TaskList
+            tasks={tasks}
+            callbackAddTask={addTask}
+            callbackChangeTaskDone={changeTaskDone}
+          />
         </div>
       ) : null}
     </div>
--- kanbanboard/frontend/src/TaskList.js
+++ kanbanboard/frontend/src/TaskList.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Task from "./Task";
 import styles from "./assets/css/TaskList.css";
 
-const TaskList = ({ cardNo, tasks, callbackAddTask }) => {
+const TaskList = ({ tasks, callbackAddTask, callbackChangeTaskDone }) => {
   return (
     <div>
       <ul>
@@ -10,9 +10,9 @@ const TaskList = ({ cardNo, tasks, callbackAddTask }) => {
           <Task
             key={task.no}
             no={task.no}
-            cardNo={cardNo}
             name={task.name}
             done={task.done}
+            callbackChangeTaskDone={callbackChangeTaskDone}
           />
         ))}
       </ul>
